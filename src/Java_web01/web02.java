1e7050a81f508aa32c719a28832dba09137dbe7b
fofehd64 <fofehd64@gmail.com>
2024-04-29 17:51:07 +0900

    Java file 수정

--- src/Java_web01/web02.java
+++ src/Java_web01/web02.java
@@ -4,8 +4,17 @@ public class web02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("KDG_Java_Web01_Test!!!!");
-		System.out.println("KDG_Java_Web01_Test!!!!");
+		final int SAFE_SPEED = 60; // 안전 속도 
+		final int MAX_VALUE = 2147483647; 
+		final int MIN_VALUE = -2147483648; 
+		final String SERVER_IP_ADDRESS = "13.124.56.189";
+		
+		
+		System.out.println("규정 속도는 " + SAFE_SPEED + "km 입니다.");
+		System.out.println("int 정수의 최대 값= " + MAX_VALUE);
+		System.out.println("int 정수의 최소 값= " + MIN_VALUE);
+		System.out.println("서버의 IP 주소는 " + SERVER_IP_ADDRESS + "입니다.");
+		System.out.println("원주율은 " + Math.PI);
 
 	}
 
--- src/Java_web01/web03.java
+++ src/Java_web01/web03.java
@@ -1,11 +1,16 @@
 package Java_web01;
 
 public class web03 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("gdgdgdgdg");
-		System.out.println("gdgdgdgdg");
+		int num = 13 ;
+		int _dong = 2;
+		String aa = "donggwan";
+		System.out.println(_dong);
+		System.out.println(num);
+		System.out.println(aa);
+	
+	
+	
 	}
-
 }
