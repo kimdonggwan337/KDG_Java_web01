1ab0e09c872a2d27b700ad8d76f7d9ff4f2c4cfa
fofehd64 <fofehd64@gmail.com>
2024-04-29 11:39:10 +0900

    cnrk

---
+++ src/Java_web01/web02.java
@@ -0,0 +1,12 @@
+package Java_web01;
+
+public class web02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("KDG_Java_Web01_Test!!!!");
+		System.out.println("KDG_Java_Web01_Test!!!!");
+
+	}
+
+}
